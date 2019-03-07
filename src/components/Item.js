0b6c43460c88e6b5590d5a6a16b1c34224452593
John Rigsby <rigsby.johnp@gmail.com>
2019-03-06 21:31:17 -0500

    Adding in a little display of a handle click

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,18 +1,36 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class List extends Component {
+    constructor (props) {
+        super(props);
 
+        this.state = {
+            hiddenProps: true
+        };
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+
+    handleClick(e) {
+        e.preventDefault();
+        this.setState(state => ({
+            hiddenProps: !this.state.hiddenProps
+        }));
+    }
 
     render() {
-
         const styles = {
-            block:{
+            block: {
 
+            },
+            propDisplay: {
+                display: (this.state.hiddenProps ? 'none' : '')
             }
         };
 
-        let cont= <div style={styles.block}><img src={this.props.image} width="50px" height="50px"/><h2>{this.props.name}</h2><p>{this.props.fact}</p></div>;
-return (cont);
+        let cont = <div style={styles.block}><img src={this.props.image} width="50px" height="50px" onClick={this.handleClick} /><h2 style={styles.propDisplay}>{this.props.name}</h2><p>{this.props.fact}</p></div>;
+        return (cont);
     }
 }
 
